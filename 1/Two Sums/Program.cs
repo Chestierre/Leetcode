3540c2af3c60371497cf785273997c4bebcc8cd6
Chestierre <chesterdoliente@gmail.com>
2025-05-26 02:59:24 +0800

    adding leetcode 1 - two sum

---
+++ 1/Two Sums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return Array.Empty<int>();
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] result = solution.TwoSum(nums, target);
+
+        if (result.Length > 0) {
+            Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+        } else {
+            Console.WriteLine("No two sum solution found.");
+        }
+    }
+}
